0024d59193d8bef21f9d4b08a7ee8fc7ca15cd83
Juan <cruzjuan298@gmail.com>
2025-01-23 13:07:25 -0500

    add mock files

--- modelTest.py
+++ modelTest.py
@@ -1,13 +1,28 @@
 from sentence_transformers import SentenceTransformer
+import faiss
+import numpy as np
 
+try: 
+    with open("mockFAQ.txt", "r") as file:
+        lines = file.readlines()
+
+except FileNotFoundError:
+    print("No file found")
+
+questions = []
+answers = []
+
+for i in range(len(lines) - 1):
+    if "?" in lines[i]:
+        question = lines[i].strip().split("**")[1]
+        answer = lines[i + 1].strip()
+        questions.append(question)
+        answers.append(answer)
 model = SentenceTransformer("sentence-transformers/all-mpnet-base-v2")
 
-responseQueries = ["Our cancelation policy is valid up until 4 weeks after your purchase date.", "If you would like to delete your account, please visit your dashboard and click on the option to delete your account.", "I am a helpful chetbot who is here to asissts you woth any questions you may have!" ]
-questionQueries = ["Would you like to see if you qualify for a cancelation?", "How are you doing today?", "How may I help you today"]
+embeddingQuestions = model.encode(questions) #encoding data to embeddings for further analysis/retrival
+embeddingResponses = model.encode(answers)
 
-embeddingResponses = model.encode(responseQueries)
-embeddingQuestions = model.encode(questionQueries)
+similartiesResponses = model.similarity(embeddingQuestions, embeddingResponses) #computing similarity scores. May not be useful for search and retrival
 
-
-similartiesResponses = model.similarity(embeddingQuestions, embeddingResponses)
 print(similartiesResponses)
