778da7559b56756e6575f3a05925c722404b1a45
Juan <cruzjuan298@gmail.com>
2025-02-06 13:59:48 -0500

    change encoding of file uplaod

--- backend/app/api/routes/upload.py
+++ backend/app/api/routes/upload.py
@@ -5,7 +5,7 @@ uploadRouter = APIRouter()
 @uploadRouter.post("/upload/")
 async def upload_document(file: UploadFile = File(...)):
     content = await file.read()
-    text = content.decode("utf-8")
+    text = content.decode("latin-1")
     from app.services.embedding import process_document
     doc_id = process_document(file.filename, text)
     return {"message" : "Document processed", "doc_id": doc_id}
--- backend/app/services/search.py
+++ backend/app/services/search.py
@@ -1,7 +1,7 @@
-from services.embedding import model 
-from services.storage import index
 
 def find_best_match(query):
+    from app.services.embedding import model 
+    from app.services.storage import index
     query_embedding = model.encode(query).astype("float32").reshape(1, -1)
     _, cloest_id = index.search(query_embedding, k=1)
     return cloest_id[0][0]
